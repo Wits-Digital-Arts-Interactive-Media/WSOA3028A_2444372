58d9a229149006dc86f33ab5108ace25f24cc073
DumisaniMc <Dumisanimofolo@gmail.com>
2024-04-15 01:41:27 +0200

    Recent Changes + Week 6 and 8 Blog update

---
+++ JavaScript Code/index.js
@@ -0,0 +1,23 @@
+//practicing my java
+function MyperfectSquares(){
+    let A = 1;
+    let x = 3;
+    let y = 5;
+    let z = 8;
+   let isTrue  = true;
+    const formular = A*A;
+    const formular1 = x*x;
+    const formular2 = y*y;
+    document.getElementById("Design.html").innerHTML ="design"
+
+    console.log(formular * formular1 % formular2 + formular2);
+    return;
+}
+MyperfectSquares();
+
+ 
+
+
+ 
+
+
